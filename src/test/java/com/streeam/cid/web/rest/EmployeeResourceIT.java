fed2d1914aee139e17e0617400ec31cb9134b783
Streeam <bogdanmihoci27@gmail.com>
2020-01-18 20:32:22 +0000

    Deploy to Heroku

--- src/test/java/com/streeam/cid/web/rest/EmployeeResourceIT.java
+++ src/test/java/com/streeam/cid/web/rest/EmployeeResourceIT.java
@@ -2,38 +2,27 @@ package com.streeam.cid.web.rest;
 
 import com.streeam.cid.CidApp;
 import com.streeam.cid.domain.Employee;
-import com.streeam.cid.domain.User;
 import com.streeam.cid.domain.Roles;
+import com.streeam.cid.domain.User;
 import com.streeam.cid.repository.EmployeeRepository;
 import com.streeam.cid.service.EmployeeService;
-import com.streeam.cid.service.dto.EmployeeDTO;
 import com.streeam.cid.service.mapper.EmployeeMapper;
 import com.streeam.cid.web.rest.errors.ExceptionTranslator;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Base64Utils;
 import org.springframework.validation.Validator;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.List;
 
 import static com.streeam.cid.web.rest.TestUtil.createFormattingConversionService;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasItem;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Integration tests for the {@link EmployeeResource} REST controller.
@@ -103,7 +92,6 @@ public class EmployeeResourceIT {
      */
     public static Employee createEntity(EntityManager em) {
         Employee employee = new Employee()
-            .hired(DEFAULT_HIRED)
             .image(DEFAULT_IMAGE)
             .imageContentType(DEFAULT_IMAGE_CONTENT_TYPE)
             .jobTitle(DEFAULT_JOB_TITLE)
@@ -129,7 +117,6 @@ public class EmployeeResourceIT {
      */
     public static Employee createUpdatedEntity(EntityManager em) {
         Employee employee = new Employee()
-            .hired(UPDATED_HIRED)
             .image(UPDATED_IMAGE)
             .imageContentType(UPDATED_IMAGE_CONTENT_TYPE)
             .jobTitle(UPDATED_JOB_TITLE)
@@ -152,221 +139,5 @@ public class EmployeeResourceIT {
         employee = createEntity(em);
     }
 
-    @Test
-    @Transactional
-    public void createEmployee() throws Exception {
-        int databaseSizeBeforeCreate = employeeRepository.findAll().size();
 
-        // Create the Employee
-        EmployeeDTO employeeDTO = employeeMapper.toDto(employee);
-        restEmployeeMockMvc.perform(post("/api/employees")
-            .contentType(TestUtil.APPLICATION_JSON_UTF8)
-            .content(TestUtil.convertObjectToJsonBytes(employeeDTO)))
-            .andExpect(status().isCreated());
-
-        // Validate the Employee in the database
-        List<Employee> employeeList = employeeRepository.findAll();
-        assertThat(employeeList).hasSize(databaseSizeBeforeCreate + 1);
-        Employee testEmployee = employeeList.get(employeeList.size() - 1);
-        assertThat(testEmployee.isHired()).isEqualTo(DEFAULT_HIRED);
-        assertThat(testEmployee.getImage()).isEqualTo(DEFAULT_IMAGE);
-        assertThat(testEmployee.getImageContentType()).isEqualTo(DEFAULT_IMAGE_CONTENT_TYPE);
-        assertThat(testEmployee.getJobTitle()).isEqualTo(DEFAULT_JOB_TITLE);
-        assertThat(testEmployee.getHiredDate()).isEqualTo(DEFAULT_HIRED_DATE);
-    }
-
-    @Test
-    @Transactional
-    public void createEmployeeWithExistingId() throws Exception {
-        int databaseSizeBeforeCreate = employeeRepository.findAll().size();
-
-        // Create the Employee with an existing ID
-        employee.setId(1L);
-        EmployeeDTO employeeDTO = employeeMapper.toDto(employee);
-
-        // An entity with an existing ID cannot be created, so this API call must fail
-        restEmployeeMockMvc.perform(post("/api/employees")
-            .contentType(TestUtil.APPLICATION_JSON_UTF8)
-            .content(TestUtil.convertObjectToJsonBytes(employeeDTO)))
-            .andExpect(status().isBadRequest());
-
-        // Validate the Employee in the database
-        List<Employee> employeeList = employeeRepository.findAll();
-        assertThat(employeeList).hasSize(databaseSizeBeforeCreate);
-    }
-
-
-    @Test
-    @Transactional
-    public void checkHiredIsRequired() throws Exception {
-        int databaseSizeBeforeTest = employeeRepository.findAll().size();
-        // set the field null
-        employee.setHired(null);
-
-        // Create the Employee, which fails.
-        EmployeeDTO employeeDTO = employeeMapper.toDto(employee);
-
-        restEmployeeMockMvc.perform(post("/api/employees")
-            .contentType(TestUtil.APPLICATION_JSON_UTF8)
-            .content(TestUtil.convertObjectToJsonBytes(employeeDTO)))
-            .andExpect(status().isBadRequest());
-
-        List<Employee> employeeList = employeeRepository.findAll();
-        assertThat(employeeList).hasSize(databaseSizeBeforeTest);
-    }
-
-    @Test
-    @Transactional
-    public void getAllEmployees() throws Exception {
-        // Initialize the database
-        employeeRepository.saveAndFlush(employee);
-
-        // Get all the employeeList
-        restEmployeeMockMvc.perform(get("/api/employees?sort=id,desc"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-            .andExpect(jsonPath("$.[*].id").value(hasItem(employee.getId().intValue())))
-            .andExpect(jsonPath("$.[*].hired").value(hasItem(DEFAULT_HIRED.booleanValue())))
-            .andExpect(jsonPath("$.[*].imageContentType").value(hasItem(DEFAULT_IMAGE_CONTENT_TYPE)))
-            .andExpect(jsonPath("$.[*].image").value(hasItem(Base64Utils.encodeToString(DEFAULT_IMAGE))))
-            .andExpect(jsonPath("$.[*].jobTitle").value(hasItem(DEFAULT_JOB_TITLE)))
-            .andExpect(jsonPath("$.[*].hiredDate").value(hasItem(DEFAULT_HIRED_DATE.toString())));
-    }
-
-    @Test
-    @Transactional
-    public void getEmployee() throws Exception {
-        // Initialize the database
-        employeeRepository.saveAndFlush(employee);
-
-        // Get the employee
-        restEmployeeMockMvc.perform(get("/api/employees/{id}", employee.getId()))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-            .andExpect(jsonPath("$.id").value(employee.getId().intValue()))
-            .andExpect(jsonPath("$.hired").value(DEFAULT_HIRED.booleanValue()))
-            .andExpect(jsonPath("$.imageContentType").value(DEFAULT_IMAGE_CONTENT_TYPE))
-            .andExpect(jsonPath("$.image").value(Base64Utils.encodeToString(DEFAULT_IMAGE)))
-            .andExpect(jsonPath("$.jobTitle").value(DEFAULT_JOB_TITLE))
-            .andExpect(jsonPath("$.hiredDate").value(DEFAULT_HIRED_DATE.toString()));
-    }
-
-    @Test
-    @Transactional
-    public void getNonExistingEmployee() throws Exception {
-        // Get the employee
-        restEmployeeMockMvc.perform(get("/api/employees/{id}", Long.MAX_VALUE))
-            .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @Transactional
-    public void updateEmployee() throws Exception {
-        // Initialize the database
-        employeeRepository.saveAndFlush(employee);
-
-        int databaseSizeBeforeUpdate = employeeRepository.findAll().size();
-
-        // Update the employee
-        Employee updatedEmployee = employeeRepository.findById(employee.getId()).get();
-        // Disconnect from session so that the updates on updatedEmployee are not directly saved in db
-        em.detach(updatedEmployee);
-        updatedEmployee
-            .hired(UPDATED_HIRED)
-            .image(UPDATED_IMAGE)
-            .imageContentType(UPDATED_IMAGE_CONTENT_TYPE)
-            .jobTitle(UPDATED_JOB_TITLE)
-            .hiredDate(UPDATED_HIRED_DATE);
-        EmployeeDTO employeeDTO = employeeMapper.toDto(updatedEmployee);
-
-        restEmployeeMockMvc.perform(put("/api/employees")
-            .contentType(TestUtil.APPLICATION_JSON_UTF8)
-            .content(TestUtil.convertObjectToJsonBytes(employeeDTO)))
-            .andExpect(status().isOk());
-
-        // Validate the Employee in the database
-        List<Employee> employeeList = employeeRepository.findAll();
-        assertThat(employeeList).hasSize(databaseSizeBeforeUpdate);
-        Employee testEmployee = employeeList.get(employeeList.size() - 1);
-        assertThat(testEmployee.isHired()).isEqualTo(UPDATED_HIRED);
-        assertThat(testEmployee.getImage()).isEqualTo(UPDATED_IMAGE);
-        assertThat(testEmployee.getImageContentType()).isEqualTo(UPDATED_IMAGE_CONTENT_TYPE);
-        assertThat(testEmployee.getJobTitle()).isEqualTo(UPDATED_JOB_TITLE);
-        assertThat(testEmployee.getHiredDate()).isEqualTo(UPDATED_HIRED_DATE);
-    }
-
-    @Test
-    @Transactional
-    public void updateNonExistingEmployee() throws Exception {
-        int databaseSizeBeforeUpdate = employeeRepository.findAll().size();
-
-        // Create the Employee
-        EmployeeDTO employeeDTO = employeeMapper.toDto(employee);
-
-        // If the entity doesn't have an ID, it will throw BadRequestAlertException
-        restEmployeeMockMvc.perform(put("/api/employees")
-            .contentType(TestUtil.APPLICATION_JSON_UTF8)
-            .content(TestUtil.convertObjectToJsonBytes(employeeDTO)))
-            .andExpect(status().isBadRequest());
-
-        // Validate the Employee in the database
-        List<Employee> employeeList = employeeRepository.findAll();
-        assertThat(employeeList).hasSize(databaseSizeBeforeUpdate);
-    }
-
-    @Test
-    @Transactional
-    public void deleteEmployee() throws Exception {
-        // Initialize the database
-        employeeRepository.saveAndFlush(employee);
-
-        int databaseSizeBeforeDelete = employeeRepository.findAll().size();
-
-        // Delete the employee
-        restEmployeeMockMvc.perform(delete("/api/employees/{id}", employee.getId())
-            .accept(TestUtil.APPLICATION_JSON_UTF8))
-            .andExpect(status().isNoContent());
-
-        // Validate the database contains one less item
-        List<Employee> employeeList = employeeRepository.findAll();
-        assertThat(employeeList).hasSize(databaseSizeBeforeDelete - 1);
-    }
-
-    @Test
-    @Transactional
-    public void equalsVerifier() throws Exception {
-        TestUtil.equalsVerifier(Employee.class);
-        Employee employee1 = new Employee();
-        employee1.setId(1L);
-        Employee employee2 = new Employee();
-        employee2.setId(employee1.getId());
-        assertThat(employee1).isEqualTo(employee2);
-        employee2.setId(2L);
-        assertThat(employee1).isNotEqualTo(employee2);
-        employee1.setId(null);
-        assertThat(employee1).isNotEqualTo(employee2);
-    }
-
-    @Test
-    @Transactional
-    public void dtoEqualsVerifier() throws Exception {
-        TestUtil.equalsVerifier(EmployeeDTO.class);
-        EmployeeDTO employeeDTO1 = new EmployeeDTO();
-        employeeDTO1.setId(1L);
-        EmployeeDTO employeeDTO2 = new EmployeeDTO();
-        assertThat(employeeDTO1).isNotEqualTo(employeeDTO2);
-        employeeDTO2.setId(employeeDTO1.getId());
-        assertThat(employeeDTO1).isEqualTo(employeeDTO2);
-        employeeDTO2.setId(2L);
-        assertThat(employeeDTO1).isNotEqualTo(employeeDTO2);
-        employeeDTO1.setId(null);
-        assertThat(employeeDTO1).isNotEqualTo(employeeDTO2);
-    }
-
-    @Test
-    @Transactional
-    public void testEntityFromId() {
-        assertThat(employeeMapper.fromId(42L).getId()).isEqualTo(42);
-        assertThat(employeeMapper.fromId(null)).isNull();
-    }
 }
